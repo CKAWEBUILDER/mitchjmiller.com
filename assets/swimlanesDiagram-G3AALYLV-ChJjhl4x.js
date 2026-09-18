7f7618e1a7355c847ddc14f2abe2048abd73a321
CKAWEBUILDER <clearkayakrentalsoahu@gmail.com>
2026-09-18 13:24:35 -0400

    Deploy: Pocock agentic workflow note

---
+++ assets/swimlanesDiagram-G3AALYLV-ChJjhl4x.js
@@ -0,0 +1,8 @@
+import{c as r,s as e}from"./flowDiagram-23GEKE2U-D984mhZG.js";import{_ as a}from"./mermaid.core-hyPQV7Lt.js";import"./chunk-5VM5RSS4-ChESdCda.js";import"./chunk-XXDRQBXY-C230ep2F.js";import"./chunk-VR4S4FIN-omdSbTHH.js";import"./chunk-32BRIVSS-CiaVkK6M.js";import"./channel-CXM78c2l.js";import"./index-CMkuOwUW.js";var o=a(t=>`${e(t)}
+  .swimlane.cluster rect {
+    stroke: ${t.clusterBorder} !important;
+  }
+  [data-look="neo"].cluster rect {
+    filter: none;
+  }
+`,"getStyles"),s=o,n=r({defaultLayout:"swimlane",styles:s});export{n as diagram};
